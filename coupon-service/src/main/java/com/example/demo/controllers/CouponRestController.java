f67955fbd8a3f3031cb516672aca15301cf94575
Nilay <54245048+dnilay@users.noreply.github.com>
2023-09-07 17:01:09 +0530

    Add files via upload

--- coupon-service/src/main/java/com/example/demo/controllers/CouponRestController.java
+++ coupon-service/src/main/java/com/example/demo/controllers/CouponRestController.java
@@ -1,22 +1,31 @@
 package com.example.demo.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Coupon;
 import com.example.demo.repos.CouponRepo;
 
+import lombok.AllArgsConstructor;
+
 @RestController
-@RequestMapping("/couponapi")
+@AllArgsConstructor
 public class CouponRestController {
 
-	@Autowired
-	CouponRepo repo;
+	private final CouponRepo repo;
+
+	private final Environment environment;
+
+	@GetMapping
+	public ResponseEntity<?> status() {
+		return ResponseEntity
+				.ok("coupon-service is up and runing on port: " + environment.getProperty("local.server.port"));
+	}
 
 	@PostMapping("/coupons")
 	public Coupon create(@RequestBody Coupon coupon) {
@@ -25,7 +34,7 @@ public class CouponRestController {
 
 	}
 
-	@RequestMapping(value = "/coupons/{code}", method = RequestMethod.GET)
+	@GetMapping("/coupons/{code}")
 	public Coupon getCoupon(@PathVariable("code") String code) {
 		return repo.findByCode(code);
 
--- my-product-service/src/main/java/com/example/demo/controllers/ProductRestController.java
+++ my-product-service/src/main/java/com/example/demo/controllers/ProductRestController.java
@@ -1,10 +1,10 @@
 package com.example.demo.controllers;
 
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,7 +15,7 @@ import com.example.demo.repos.ProductRepo;
 import lombok.AllArgsConstructor;
 
 @RestController
-@RequestMapping("/productapi")
+
 @AllArgsConstructor
 public class ProductRestController {
 
@@ -23,18 +23,23 @@ public class ProductRestController {
 
 	private final RestTemplate restTemplate;
 
+	private final Environment environment;
+
+	@GetMapping
+	public ResponseEntity<?> status() {
+		return ResponseEntity
+				.ok("product-service is up and runing on port: " + environment.getProperty("local.server.port"));
+	}
+
 	@PostMapping("/products")
 	public Product create(@RequestBody Product product) {
-		Coupon coupon = restTemplate.getForObject("http://localhost:8082/couponapi/coupons/" + product.getCouponCode(),
+		Coupon coupon = restTemplate.getForObject("http://localhost:9090/coupon-service/coupons/" + product.getCouponCode(),
 				Coupon.class);
 		product.setPrice(product.getPrice().subtract(coupon.getDiscount()));
 		return repo.save(product);
 
 	}
 
-	@GetMapping
-	public ResponseEntity<?> getStatus() {
-		return ResponseEntity.ok("status is up");
-	}
+	
 
 }
