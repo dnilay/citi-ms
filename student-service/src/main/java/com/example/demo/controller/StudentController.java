1174558d662be300e83dfba06f0f94de16439b80
Nilay <54245048+dnilay@users.noreply.github.com>
2023-09-08 15:24:52 +0530

    Add files via upload

--- student-service/src/main/java/com/example/demo/controller/StudentController.java
+++ student-service/src/main/java/com/example/demo/controller/StudentController.java
@@ -3,7 +3,6 @@ package com.example.demo.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +13,7 @@ import com.example.demo.dto.School;
 import com.example.demo.model.Student;
 import com.example.demo.service.StudentService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -23,18 +23,24 @@ public class StudentController {
 	private final StudentService studentService;
 
 	private SchoolClient schoolClient;
+
 	@GetMapping
-	public ResponseEntity<?> status()
-	{
+	public ResponseEntity<?> status() {
 		return ResponseEntity.ok("working");
 	}
+
 	@PostMapping("/students")
-	public ResponseEntity<?> createStudent(@RequestBody Student student,@RequestParam("schoolName") String schoolName)
-	{
-		School school=schoolClient.getSchoolByName(schoolName);
+	@CircuitBreaker(name = "student-service", fallbackMethod = "getDefaultSchool")
+	public ResponseEntity<?> createStudent(@RequestBody Student student,
+			@RequestParam("schoolName") String schoolName) {
+		School school = schoolClient.getSchoolByName(schoolName);
+		
 		student.setSchoolName(school.getSchoolName());
 		return ResponseEntity.status(HttpStatus.CREATED).body(studentService.createStudent(student));
 	}
-	
-	
+
+	public ResponseEntity<?> getDefaultSchool(Exception e)
+	{
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body( "school-service is down try after some time");
+	}
 }
